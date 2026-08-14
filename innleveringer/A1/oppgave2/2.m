eb81c450a0aeca5d2f6f3a4e6ae23611c9200811
nani1717 <nanikanblidet@gmail.com>
2026-08-14 13:11:39 +0200

    hello

--- innleveringer/A1/oppgave2/2.m
+++ innleveringer/A1/oppgave2/2.m
@@ -1,72 +1,132 @@
+% ============================================================
 % Oppgave 2
-% Oppgave 2
+%
+% Generering og sampling av en sinuskurve med ulike
+% samplingsintervaller.
+% ============================================================
 
-% a)
-dt = 5e-6; # s 
-fs = 1/dt;
-t_start = 0; # s
-t_end = 2e-3; # s
+%% Oppgave a) - Tidsvektor
 
-t = t_start:dt:t_end; 
+dt = 5e-6;                 % Tidssteg [s]
+fs = 1/dt;                 % Samplingsfrekvens [Hz]
 
-% b) 
-A = 5; # V
-f = 2e3; # Hz
-w = 2*pi*f;
+t_start = 0;               % Starttid [s]
+t_end   = 2e-3;            % Sluttid [s]
+
+t = t_start:dt:t_end;
+
+
+%% Oppgave b) - Sinussignal
+
+A = 5;                     % Amplitude [V]
+f = 2e3;                   % Frekvens [Hz]
+w = 2*pi*f;                % Vinkelfrekvens [rad/s]
 
 s = A*sin(w*t);
 
+
+% Plot med tidsakse i sekunder
 figure(1);
-subplot(2,1,1)
-plot(t, s, "lineWidth", 1.3);
 
-xlabel("time [s]"); ylabel("Amplitude [V]");
-ttl = sprintf("Sinus bølge: A = %d [V], f = %d [kHz], dt = %g [us], fs = %g [kHz], dp = %d", A, f/1e3, dt*1e6, fs/1e3, length(t)); 
+subplot(2,1,1);
+plot(t, s, "LineWidth", 1.2);
+
+xlabel("Time [s]");
+ylabel("Amplitude [V]");
+
+ttl = sprintf( ...
+    "Sinusbølge: A = %d [V], f = %g [kHz], dt = %g [us], fs = %g [kHz], dp = %d", ...
+    A, f/1e3, dt*1e6, fs/1e3, length(t));
+
 title(ttl);
+grid on;
 
-% b)
-subplot(2,1,2)
-plot(t*1e3, s, "lineWidth", 1.3);
 
-xlabel("time [ms]"); ylabel("Amplitude [V]");
-ttl = sprintf("Sinus bølge (skalert tidsakse): A = %d [V], f = %d [kHz], dt = %g [us], fs = %g [kHz], dp = %d", A, f/1e3, dt*1e6, fs/1e3, length(t)); 
+% Plot med tidsakse i millisekunder
+subplot(2,1,2);
+plot(t*1e3, s, "LineWidth", 1.2);
+
+xlabel("Time [ms]");
+ylabel("Amplitude [V]");
+
+ttl = sprintf( ...
+    "Sinusbølge, skalert tidsakse: A = %d [V], f = %g [kHz], dt = %g [us], fs = %g [kHz], dp = %d", ...
+    A, f/1e3, dt*1e6, fs/1e3, length(t));
+
 title(ttl);
+grid on;
 
-% c-f
-dts = [50e-6, 200e-6, 250e-6, 400e-6]; # steg lengder
+
+%% Oppgave c-f) - Varierende samplingsintervall
+
+dts = [50e-6, 200e-6, 250e-6, 400e-6];   % Tidssteg [s]
 colors = ["b", "r", "g", "k"];
 
-figure(3)
-hold on; 
-for i = 1:length(dts); # loope gjennom alle seg lengder
-    dt_i = dts(i);
-    fs_i = 1/dt_i;
-    t_i = t_start:dt_i:t_end; 
-    s_i = A*sin(w*t_i);
-    spp_i = fs_i/f; # sample pr periode
 
-    plot(t_i*1e3, s_i, colors(i), "lineWidth", 1.3, "DisplayName", sprintf("dt = %g us, fs = %g [kHz], spp = %d, dp = %d", dt_i*1e6, fs_i/1e3, spp_i, length(t_i)));
-end;
+% Alle signalene i samme figur
+figure(3);
+hold on;
 
-ttl = sprintf("Sinus bølge varierende dt: A = %d [V], f = %d [kHz]", A, f/1e3); 
-title(ttl);
-xlabel("time [ms]"); ylabel("Amplitude [V]");
-legend();
-
-hold off;
-
-figure(4);
 for i = 1:length(dts)
     dt_i = dts(i);
     fs_i = 1/dt_i;
-    t_i = t_start:dt_i:t_end; 
+
+    t_i = t_start:dt_i:t_end;
     s_i = A*sin(w*t_i);
-    spp_i = fs_i/f; # sample pr periode
 
-    subplot(2,2, i);
-    plot(t_i*1e3, s_i, colors(i), "lineWidth", 1.3);
+    spp_i = fs_i/f;        % Samples per periode
 
-    xlabel("time [ms]"); ylabel("Amplitude [V]");
-    ttl_i = sprintf("Sinus bølge: dt = %g [us], fs = %g [kHz], spp = %d, dp = %d", dt_i*1e6, fs_i/1e3, spp_i, length(t_i));
+    dsp = sprintf( ...
+        "dt = %g us, fs = %g [kHz], spp = %g, dp = %d", ...
+        dt_i*1e6, fs_i/1e3, spp_i, length(t_i));
+
+    plot( ...
+        t_i*1e3, ...
+        s_i, ...
+        colors(i), ...
+        "LineWidth", 1.2, ...
+        "DisplayName", dsp);
+end
+
+title(sprintf( ...
+    "Sinusbølge med varierende dt: A = %d [V], f = %g [kHz]", ...
+    A, f/1e3));
+
+xlabel("Time [ms]");
+ylabel("Amplitude [V]");
+
+legend();
+grid on;
+hold off;
+
+
+% Hvert signal i eget subplot
+figure(4);
+
+for i = 1:length(dts)
+    dt_i = dts(i);
+    fs_i = 1/dt_i;
+
+    t_i = t_start:dt_i:t_end;
+    s_i = A*sin(w*t_i);
+
+    spp_i = fs_i/f;        % Samples per periode
+
+    subplot(2,2,i);
+
+    plot( ...
+        t_i*1e3, ...
+        s_i, ...
+        colors(i), ...
+        "LineWidth", 1.2);
+
+    xlabel("Time [ms]");
+    ylabel("Amplitude [V]");
+
+    ttl_i = sprintf( ...
+        "dt = %g [us], fs = %g [kHz], spp = %g, dp = %d", ...
+        dt_i*1e6, fs_i/1e3, spp_i, length(t_i));
+
     title(ttl_i);
-end;
+    grid on;
+end
